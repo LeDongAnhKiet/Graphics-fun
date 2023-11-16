2eab465538a5c288618bea03ae865736c19cc84d
Kiệt <2051052068kiet@ou.edu.vn>
2023-11-16 17:01:48 +0700

    fix cube

--- spiningCube.cpp
+++ spiningCube.cpp
@@ -1,86 +1,114 @@
+﻿#include <iostream>
 #include <cmath>
-#include <iostream>
-#include <string>
-#include <thread>
-#include <chrono>
+#include <cstring>
+//#include <thread>
+//#include <chrono>
+using namespace std;
 
-float A, B, C, X, Y, Z, Z1, K1;
+float A, B, C, x, y, z;
 
-float canh = 20, zBuffer[160 * 44], tocDo = 0.6;
+float canh = 20, offset, K0 = 40;
 int rong = 160, cao = 44, xa = 100;
+float zBuffer[160 * 44], tocDo = 0.6, Z0;
 char buffer[160 * 44];
-int ascii = ' ', xp, yp, idx;
+int nenAscii = ' ', X0, Y0, i;
 
-float toadoX (int i, int j, int k)
+float toadoX(int i, int j, int k)
 {
-	return j * sin(A) * sin(B) * cos(C)
-		- k * cos(A) * sin(B) * cos(C) + j * cos(A) * sin(C)
-		+ k * sin(A) * sin(C) + i * cos(B) * cos(C);
+    return j * sin(A) * sin(B) * cos(C)
+        - k * cos(A) * sin(B) * cos(C) + j * cos(A) * sin(C)
+        + k * sin(A) * sin(C) + i * cos(B) * cos(C);
 }
 
-float toadoY (int i, int j, int k)
+float toadoY(int i, int j, int k)
 {
-	return j * cos(A) * cos(C) + k * sin(A) * cos(C)
-		- j * sin(A) * sin(B) * sin(C)
-		+ k * cos(A) * sin(B) * sin(C) - i * cos(B) * sin(C);
+    return j * cos(A) * cos(C) + k * sin(A) * cos(C)
+        - j * sin(A) * sin(B) * sin(C)
+        + k * cos(A) * sin(B) * sin(C) - i * cos(B) * sin(C);
 }
 
 float toadoZ(int i, int j, int k)
 {
-	return k * cos(A)* cos(B) - j * sin(A) * cos(B) + i * sin(B);
+    return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
 }
 
-void beMat(float x, float y, float z, int ch)
+void beMat(float X, float Y, float Z, int ch)
 {
-	X = toadoX(x, y, z);
-	Y = toadoY(x, y, z);
-	Z = toadoZ(x, y, z) + xa;
+    x = toadoX(X, Y, Z);
+    y = toadoY(X, Y, Z);
+    z = toadoZ(X, Y, Z) + xa;
 
-	Z1 = 1 / Z;
-	xp = (int)(rong / 2 - 2 * canh + K1 * Z1 * X * 2);
-	yp = (int)(cao / 2 + K1 * Z1 * Y);
-	idx = xp + yp * rong;
+    Z0 = 1 / z;
+    X0 = static_cast<int>(rong / 2 + offset + K0 * Z0 * x * 2);
+    Y0 = static_cast<int>(cao / 2 + K0 * Z0 * y);
 
-	if (idx >= 0 && idx < rong * cao)
-	{
-		if (Z1 > zBuffer[idx])
-		{
-			zBuffer[idx] = Z1;
-			buffer[idx] = ch;
-		}
-	}
+    i = X0 + Y0 * rong;
+    if (i >= 0 && i < rong * cao)
+        if (Z0 > zBuffer[i])
+        {
+            zBuffer[i] = Z0;
+            buffer[i] = ch;
+        }
 }
 
 int main()
 {
-	std::cout << "\x1b[2J";
-	while (true)
-	{
-		std::memset(buffer, ascii, rong * cao);
-		std::memset(zBuffer, 0, rong * cao * 4);
-		
-		for (float X = -canh; X < canh; X += tocDo)
-		{
-			for (float Y = -canh; Y < canh; Y += tocDo)
-			{
-				beMat(X, Y, -canh, '#');
-				beMat(canh, Y, X, '$');
-				beMat(-canh, Y, -X, '@');
-				beMat(-X, Y, canh, '%');
-				beMat(X, -canh, X - Y, '&');
-				beMat(X, canh, Y, '*');
-			}
-		}
-		
-		std::cout << "\x1b[H";
-		for (int k = 0; k < rong * cao; k++)
-		{
-			std::putchar(k % rong ? buffer[k] : 10);
-		}
-		
-		A += 0.005;
-		B += 0.005;
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-	}
-	return 0;
+    cout << "\x1b[2J";
+    while (true)
+    {
+        memset(buffer, nenAscii, rong * cao);
+        memset(zBuffer, 0, rong * cao * 4);
+
+        canh = 20;
+        offset = -2 * canh;
+        // hình lớn
+        for (float X = -canh; X < canh; X += tocDo)
+            for (float Y = -canh; Y < canh; Y += tocDo)
+            {
+                beMat(X, Y, -canh, '@');
+                beMat(canh, Y, X, '$');
+                beMat(-canh, Y, -X, '~');
+                beMat(-X, Y, canh, '#');
+                beMat(X, -canh, -Y, ';');
+                beMat(X, canh, Y, '+');
+            }
+
+        canh = 10;
+        offset = 1 * canh;
+        // hình giữa
+        for (float X = -canh; X < canh; X += tocDo)
+            for (float Y = -canh; Y < canh; Y += tocDo)
+            {
+                beMat(X, Y, -canh, '@');
+                beMat(canh, Y, X, '$');
+                beMat(-canh, Y, -X, '~');
+                beMat(-X, Y, canh, '#');
+                beMat(X, -canh, -Y, ';');
+                beMat(X, canh, Y, '+');
+            }
+
+        canh = 5;
+        offset = 8 * canh;
+        // hình cuối
+        for (float X = -canh; X < canh; X += tocDo)
+            for (float Y = -canh; Y < canh; Y += tocDo)
+            {
+                beMat(X, Y, -canh, '@');
+                beMat(canh, Y, X, '$');
+                beMat(-canh, Y, -X, '~');
+                beMat(-X, Y, canh, '#');
+                beMat(X, -canh, -Y, ';');
+                beMat(X, canh, Y, '+');
+            }
+
+        cout << "\x1b[H";
+        for (int k = 0; k < rong * cao; k++)
+            putchar(k % rong ? buffer[k] : 10);
+
+        A += 0.05;
+        B += 0.05;
+        C += 0.01;
+        //this_thread::sleep_for(chrono::seconds(1));
+    }
+    return 0;
 }
