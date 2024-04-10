c5147b6bfdcd84761374292ff77ae13b9477a7b8
kietlda <kietlda@enjoysport.vn>
2024-04-10 16:36:51 +0700

    optimized

--- galaxies/app.js
+++ galaxies/app.js
@@ -18,7 +18,6 @@ function createScene() {
   renderer.shadowMap.enabled = true;
 
   document.getElementById('scene').appendChild(renderer.domElement);
-
   addListeners();
   controls = new THREE.OrbitControls(camera, renderer.domElement);
 }
@@ -53,8 +52,7 @@ function vortex() {
 
       if (params.vortex > 0)
         angle *= (1 - vertex.length() / params.radius) * params.vortex;
-      else
-        angle *= (vertex.length() / params.radius) * Math.abs(params.vortex);
+      else angle *= (vertex.length() / params.radius) * Math.abs(params.vortex);
 
       vertex.applyAxisAngle(axis, angle);
     }
@@ -89,30 +87,14 @@ function sprite() {
     canvas.height / 2,
     canvas.width / 2
   );
-
-  gradient.addColorStop(0, 'rgba(255,255,255,1)');
-  gradient.addColorStop(0.2, 'rgba(240,255,240,1)');
-  gradient.addColorStop(0.22, 'rgba(0,150,255,.2)');
-  gradient.addColorStop(1, 'rgba(0,50,255,0)');
+  gradient.addColorStop(0,    'rgba (255, 255,  255, 1)');
+  gradient.addColorStop(0.2,  'rgba (240, 255,  240, 1)');
+  gradient.addColorStop(0.22, 'rgba (0,   150,  255, .2)');
+  gradient.addColorStop(1,    'rgba (0,   50,   255, 0)');
 
   ctx.fillStyle = gradient;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-
   return canvas;
-}
-
-function bufferGeometry() {
-  const geometry = new THREE.BufferGeometry();
-  const vertices = new Float32Array([
-    -1.0, -1.0, 1.0,
-    1.0, -1.0, 1.0,
-    1.0, 1.0, 1.0,
-    1.0, 1.0, 1.0,
-    -1.0, 1.0, 1.0,
-    -1.0, -1.0, 1.0
-  ]);
-
-  geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 3));
 }
 
 function createObjects() {
@@ -126,7 +108,6 @@ function createObjects() {
     depthTest: false,
     transparent: true
   });
-
   particles = new THREE.Points(geometry, material);
   scene.add(particles);
 }
@@ -145,151 +126,25 @@ function onChange(params) {
     let pow = Math.max(0, ((params.radius * 0.8) - distance) / (params.radius * 0.8));
     pow = (1 - Math.cos(pow * Math.PI)) * params.bulge;
 
-    let vertex = new THREE.Vector3(point.x, point.y, 0);
+    let vertex = new THREE.Vector3(point.x, point.y, (-dz + (dz * 2) * Math.random()) * pow);
     geometry.vertices.push(vertex);
     geometry.colors.push(new THREE.Color(pow, pow, 1));
 
     let t = Math.round(pow * 5), n = 0;
     while (n < t) {
-      let vertex = new THREE.Vector3(point.x - dx + Math.random() * (dx * 2),
+      let vertex = new THREE.Vector3(
+        point.x - dx + Math.random() * (dx * 2),
         point.y - dy + Math.random() * (dy * 2),
-        0);
+        (-dz + (dz * 2) * Math.random()) * pow
+      );
       geometry.vertices.push(vertex);
       geometry.colors.push(new THREE.Color(pow, pow, 1));
       n++;
     }
   }
-
   geometry.mergeVertices();
   geometry.verticesNeedUpdate = true;
   particles.geometry = geometry;
-}
-
-function spiral(params) {
-  function lcg(value, modulus = Math.pow(2, 31), multiplier = 1103515245, increment = 12345) {
-    return (value * multiplier + increment) % modulus;
-  }
-
-  return {
-    toArray: function (now = 0) {
-      const time = now / -60000;
-      const modulus = params.modulus();
-      const theta = params.armTheta();
-      let points = [];
-      let value = 0;
-
-      for (let arm = 0; arm < params.arms; arm++)
-        for (let stop = 0; stop < params.stops; stop++) {
-          value = lcg(value, modulus);
-          const pow = (stop / params.stops);
-          const c = 1 - pow + 1 - params.dispersion;
-          const r = value / modulus;
-          const cr = (r - 0.5) * 2;
-          const angle = (pow * Math.PI * 2 * params.revolutions) + (theta * arm);
-          const radians = time + angle + (Math.PI * c * cr * params.sparse);
-          const distance = Math.sqrt(pow) * params.radius;
-          const x = Math.cos(radians) * distance;
-          const y = Math.sin(radians) * distance;
-          const z = 0;
-          const size = (r - 0.5) * 2 + Math.pow(1.125, (1 - pow) * 8);
-          const alpha = (Math.sin((r + time + pow) * Math.PI * 2) + 1) * 0.5;
-
-          points.push({
-            x: x, y: y, z: z,
-            size: size, alpha: alpha,
-          });
-        }
-
-      return points;
-    },
-  };
-}
-
-function downloadFile(data, fileName, json) {
-  data = json ? JSON.stringify(data) : data;
-  const blob = new Blob([data], { type: 'octet/stream' });
-  const url = window.URL.createObjectURL(blob);
-
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = fileName;
-  a.click();
-
-  window.URL.revokeObjectURL(url);
-}
-
-function string2ArrayBuffer(string, callback) {
-  const blob = new Blob([string]);
-  const fr = new FileReader();
-
-  fr.onload = function (e) {
-    callback(e.target.result);
-  };
-
-  fr.readAsArrayBuffer(blob);
-}
-
-function PCDExporter(vertices) {
-  let data = '# .PCD v.7 - Point Cloud Data file format\r\n';
-  data += 'VERSION .7\r\n';
-  data += 'FIELDS x y z rgb\r\n';
-  data += 'SIZE 4 4 4 4\r\n';
-  data += 'TYPE F F F F\r\n';
-  data += 'COUNT 1 1 1 1\r\n';
-  data += 'WIDTH ' + vertices.length + '\r\n';
-  data += 'HEIGHT 1\r\n';
-  data += 'VIEWPOINT 0 0 0 1 0 0 0\r\n';
-  data += 'POINTS ' + vertices.length + '\r\n';
-  data += 'DATA ascii\r\n';
-
-  for (let i = 0; i < vertices.length; i++) {
-    let v = vertices[i];
-    let x = v.x.toFixed(5);
-    let y = v.y.toFixed(5);
-    let z = v.z.toFixed(5);
-    data += x + ' ' + y + ' ' + z + ' 4.2108e+06';
-    if (i < vertices.length - 1)
-      data += '\r\n';
-  }
-
-  return data;
-}
-
-function PLYExporter(vertices) {
-  let model = {
-    vertex: {
-      x: [],
-      y: [],
-      z: [],
-    },
-    face: {
-      vertex_index: [],
-    },
-  };
-
-  for (let i = 0; i < vertices.length; i++) {
-    let v = vertices[i];
-    model.vertex.x.push(v.x);
-    model.vertex.y.push(v.y);
-    model.vertex.z.push(v.z);
-  }
-
-  let data = writePLY(model);
-  return data;
-}
-
-function XYZExporter(vertices) {
-  let data = '';
-
-  for (let i = 0; i < vertices.length; i++) {
-    let v = vertices[i];
-    let x = v.x.toFixed(6);
-    let y = v.y.toFixed(6);
-    let z = v.z.toFixed(6);
-    data += x + ' ' + y + ' ' + z + '\r\n';
-  }
-
-  return data;
 }
 
 function init() {
@@ -304,15 +159,15 @@ onChange(params);
 let gui = (function datgui() {
   let gui = new dat.GUI();
   gui.closed = true;
-  gui.add(params, 'arms', 1, 10).onChange(onChange);
-  gui.add(params, 'stops', 1000, 10000).onChange(onChange);
-  gui.add(params, 'revolutions', 1.1, 3.1).onChange(onChange);
-  gui.add(params, 'radius', 300, 1000).onChange(onChange);
-  gui.add(params, 'sparse', 0.1, 1).onChange(onChange);
-  gui.add(params, 'dispersion', 0.01, 1).onChange(onChange);
-  gui.add(params, 'bulge', 0.01, 1).onChange(onChange);
-  gui.add(params, 'vortex', -1, 1, 0.01).onChange(onChange);
+  gui.add(params, 'arms',         1,    10)     .onChange (onChange);
+  gui.add(params, 'stops',        1000, 10000)  .onChange (onChange);
+  gui.add(params, 'revolutions',  1.1,  3.1)    .onChange (onChange);
+  gui.add(params, 'radius',       300,  1000)   .onChange (onChange);
+  gui.add(params, 'sparse',       0.1,  1)      .onChange (onChange);
+  gui.add(params, 'dispersion',   0.01, 1)      .onChange (onChange);
+  gui.add(params, 'bulge',        0.01, 1)      .onChange (onChange);
+  gui.add(params, 'vortex',       -1,   1, 0.01).onChange (onChange);
   gui.add(params, 'randomize');
-  gui.add(params, 'exportXYZ').name('EXPORT *.XYZ');
+  gui.add(params, 'exportXYZ')                .name ('EXPORT *.XYZ');
   return gui;
-})();
+}) ();
